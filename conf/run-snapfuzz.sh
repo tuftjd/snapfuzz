ef48bc8ecee209ff31563e00048190515bf8810d
tuftjd <tuftjd@qq.com>
2024-06-17 20:42:32 +0800

    Initial commit

--- conf/run-snapfuzz.sh
+++ conf/run-snapfuzz.sh
@@ -1,49 +1,5 @@
 #!/bin/bash
 
-set -eu
-trap 'exit 130' INT
-
-DEBUG=0
-LONG_RUN=0
-SHORT_RUN=0
-
-while getopts "dls" opt; do
-  case $opt in
-  d)
-    echo "Debug mode ON"
-    DEBUG=1
-    ;;
-  l)
-    echo "24h long run ON"
-    LONG_RUN=1
-    ;;
-  s)
-    echo "1k iterations run ON"
-    SHORT_RUN=1
-    ;;
-  *)
-    echo "Uknown flag!"
-    exit 1
-    ;;
-  esac
-done
-shift $((OPTIND - 1))
-
-if [ "${LONG_RUN}" == 1 ]; then
-  AFL_BINS=("./afl-fuzz-noaffin-long")
-  AFL_WRAPPER="timeout --preserve-status --signal=SIGINT --kill-after=10 24h"
-elif [ "${SHORT_RUN}" == 1 ]; then
-  AFL_BINS=("./afl-fuzz-noaffin-short")
-  AFL_WRAPPER=""
-else
-  AFL_BINS=("./afl-fuzz-noaffin")
-  # AFL_BINS=("./afl-fuzz-noaffin" "./afl-fuzz-noaffin-print")
-  AFL_WRAPPER=""
-fi
-
-SBR_BINS=("sabre")
-SBR_PLUGINS=("libsbr-afl.so")
-
 case "$1" in
 dicom)
   PROJECT_NAME="dicom"
@@ -132,70 +88,7 @@ rtsp-asan)
   ;;
 esac
 
-ALL_BINS=("${AFL_BINS[@]}" "${TARGET_BINS[@]}" "${SBR_BINS[@]}" "${SBR_PLUGINS[@]}")
-for BIN in "${ALL_BINS[@]}"; do
-  if [ ! -f "${BIN}" ]; then
-    echo "${BIN} is missing."
-    exit 1
-  fi
-done
 
-if [ "$(find . -maxdepth 1 -name "aflout-${PROJECT_NAME}-*" | wc -l)" -ne 0 ]; then
-  echo "Aflout directories alredy exists. (Re)move them to continue."
-  exit 1
-fi
-if [ "$(find . -maxdepth 1 -name "output-${PROJECT_NAME}-*" | wc -l)" -ne 0 ]; then
-  echo "Output files alredy exists. (Re)move them to continue."
-  exit 1
-fi
+CMD="my-afl -A ./libsbr-afl.so ${AFL_ARGS} -o $1 ${TARGET_BINS} ${TARGET_CONF} "
+echo $CMD
 
-echo "++++++++++++++++++++++++++++ STATS ++++++++++++++++++++++++++++"
-echo "PWD: $(pwd)"
-echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-
-MAX_TRY=9
-if [ "${SHORT_RUN}" == 1 ]; then
-  MAX_TRY=2
-fi
-
-IT=1
-for AFL_BIN in "${AFL_BINS[@]}"; do
-  for TARGET_BIN in "${TARGET_BINS[@]}"; do
-    for ((TRY=0; TRY <= MAX_TRY; TRY++)); do
-      ALF_RESULT_DIR="aflout-${PROJECT_NAME}-${IT}.${TRY}"
-      OUTPUT_FILE="output-${PROJECT_NAME}-${IT}.${TRY}.txt"
-
-      echo "Running ${IT}.${TRY}: ${AFL_BIN} + ${TARGET_BIN}"
-      echo "Output: ${OUTPUT_FILE}, ${ALF_RESULT_DIR}"
-      CMD="${AFL_WRAPPER} ${AFL_BIN} -A ./libsbr-afl.so ${AFL_ARGS} -o ./${ALF_RESULT_DIR} ${TARGET_BIN} ${TARGET_CONF} &>./${OUTPUT_FILE}"
-
-      if [ "${DEBUG}" == 1 ]; then
-        echo "${CMD}"
-        exit 1
-      else
-        # eval "AFL_BENCH_JUST_ONE=1 ${CMD}"
-        eval "${CMD}" || true
-      fi
-      echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-    done
-    ((IT = IT + 1))
-  done
-done
-
-TIMENOW=$(date +%Y-%m-%d-%H-%M)
-RESULTS_DIR="results-snapfuzz-${PROJECT_NAME}-${TIMENOW}"
-mkdir "${RESULTS_DIR}"
-mv aflout-${PROJECT_NAME}-* output-${PROJECT_NAME}-* "${RESULTS_DIR}"
-
-# Check for errors
-OUTPUT_FILES=$(ls -1q "${RESULTS_DIR}/output-${PROJECT_NAME}-"* | wc -l)
-CHECK_1=$(grep "We're done here. Have a nice day!" "${RESULTS_DIR}/output-${PROJECT_NAME}-"* | wc -l)
-CHECK_2=$(grep "All set and ready to roll!" "${RESULTS_DIR}/output-${PROJECT_NAME}-"* | wc -l)
-
-if [ "${OUTPUT_FILES}" != "${CHECK_1}" ] || [ "${OUTPUT_FILES}" != "${CHECK_2}" ]; then
-  echo "There was a problem in the run. Please check the ${RESULTS_DIR}/output-${PROJECT_NAME} files for more!"
-  exit 1
-fi
-
-echo "Results in: ${RESULTS_DIR}"
-echo "Done!"
---
+++ run.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+arg1=$1
+cur=$(date +"%Y-%m-%d-%H:%M:%S")
+out_dir="../snap_exp/snap_${arg1}_$cur"
+
+case "$1" in
+dicom)
+  PROJECT_NAME="dicom"
+  AFL_ARGS="-m 512 -i ./conf/in-dicom -P DICOM -E -K -R"
+  TARGET_CONF=""
+  TARGET_BINS=("./dcmqrscp")
+  export DCMDICTPATH="./conf/dicom.dic"
+  ;;
+dicom-asan)
+  PROJECT_NAME="dicom-asan"
+  AFL_ARGS="-t 1000 -m none -i ./conf/in-dicom -P DICOM -E -K -R"
+  TARGET_CONF=""
+  TARGET_BINS=("./dcmqrscp-asan")
+  export DCMDICTPATH="./conf/dicom.dic"
+  ;;
+
+dns)
+  PROJECT_NAME="dns"
+  AFL_ARGS="-m none -i ./conf/in-dns -P DNS -K -R"
+  TARGET_CONF="-C ./conf/dnsmasq.conf"
+  TARGET_BINS=("./dnsmasq")
+  ;;
+dns-asan)
+  PROJECT_NAME="dns-asan"
+  AFL_ARGS="-t 1000 -m none -i ./conf/in-dns -P DNS -K -R"
+  TARGET_CONF="-C ./conf/dnsmasq.conf"
+  TARGET_BINS=("./dnsmasq-asan")
+  ;;
+
+dtls)
+  PROJECT_NAME="dtls"
+  AFL_ARGS="-m 512 -i ./conf/in-dtls -P DTLS12 -q 3 -s 3 -E -K -R"
+  TARGET_CONF=""
+  TARGET_BINS=("./dtls-server")
+  ;;
+dtls-asan)
+  PROJECT_NAME="dtls-asan"
+  AFL_ARGS="-t 1000 -m none -i ./conf/in-dtls -P DTLS12 -q 3 -s 3 -E -K -R"
+  TARGET_CONF=""
+  TARGET_BINS=("./dtls-server-asan")
+  ;;
+
+ftp)
+  PROJECT_NAME="ftp"
+  AFL_ARGS="-m 512 -i ./conf/in-ftp -x ./conf/ftp.dict -P FTP -t 10000 -q 3 -s 3 -E -R"
+  TARGET_CONF="./conf/fftp.conf 2200"
+  # TARGET_BINS=("./fftp" "./fftp-pthreadjoin")
+  TARGET_BINS=("./fftp")
+  ;;
+ftp-asan)
+  PROJECT_NAME="ftp-asan"
+  AFL_ARGS="-t 1000 -m none -i ./conf/in-ftp -x ./conf/ftp.dict -P FTP -q 3 -s 3 -E -R"
+  TARGET_CONF="./conf/fftp.conf 2200"
+  TARGET_BINS=("./fftp-asan")
+  ;;
+ftp-tsan)
+  PROJECT_NAME="ftp-tsan"
+  AFL_ARGS="-t 1000 -m none -i ./conf/in-ftp -x ./conf/ftp.dict -P FTP -q 3 -s 3 -E -R"
+  TARGET_CONF="./conf/fftp.conf 2200"
+  TARGET_BINS=("./fftp-tsan")
+  ;;
+ftp-pth)
+  PROJECT_NAME="ftp-pth"
+  AFL_ARGS="-m 512 -i ./conf/in-ftp -x ./conf/ftp.dict -P FTP -q 3 -s 3 -E -R"
+  TARGET_CONF="./conf/fftp.conf 2200"
+  TARGET_BINS=("./fftp")
+  export AFL_PRELOAD="./lib2pthread.so"
+  ;;
+
+rtsp)
+  PROJECT_NAME="rtsp"
+  AFL_ARGS="-m 512 -i ./conf/in-rtsp -x ./conf/rtsp.dict -P RTSP -q 3 -s 3 -E -K -R"
+  TARGET_CONF="8554"
+  TARGET_BINS=("./testOnDemandRTSPServer")
+  ;;
+rtsp-asan)
+  PROJECT_NAME="rtsp-asan"
+  AFL_ARGS="-t 1000 -m none -i ./conf/in-rtsp -x ./conf/rtsp.dict -P RTSP -q 3 -s 3 -E -K -R"
+  TARGET_CONF="8554"
+  TARGET_BINS=("./testOnDemandRTSPServer-asan")
+  ;;
+
+*)
+  echo "Unknown command. Try one of {dicom,dns,dtls,ftp,rtsp}"
+  exit 1
+  ;;
+esac
+
+
+CMD="my-afl -A ./libsbr-afl.so ${AFL_ARGS} -o ${out_dir} ${TARGET_BINS} ${TARGET_CONF} "
+eval $CMD
+
